aa66f815e4c94c48cbafdfd332e02c3f3c41fe2b
Jamis <hoojamis@gmail.com>
2015-07-19 22:55:15 +0800

    update nexflix movie

--- src/netflix_movie.h
+++ src/netflix_movie.h
@@ -17,6 +17,7 @@
 
 #include <cstdlib>
 #include <string>
+#include <cmath>
 #include <vector>
 
 class Movie {
@@ -41,6 +42,21 @@ public:
         }
     }
 
+    std::string to_string() const { 
+        std::string str;
+        str += to_hex_string(movie_id());
+        str += ":";
+        for (size_t i = 0; i < num_users(); ++i) {
+            str += to_hex_string(user_id(i));
+            str += ',';
+            str += to_hex_string(rating(i));
+            str += ',';
+        }
+        if (str.back() == ',') str.pop_back();
+        
+        return str;
+    }
+        
     uint32_t movie_id() const { return movie_id_; }
     size_t num_users() const { return user_ids_.size(); }
     uint32_t user_id(size_t i) const { return user_ids_[i]; }
@@ -63,7 +79,48 @@ public:
         return count;
     }
 
+    float cos_distance(const Movie& mv) const {
+        auto first1 = user_ids_.begin();
+        auto last1 = user_ids_.end();
+        auto first2 = mv.user_ids_.begin();
+        auto last2 = mv.user_ids_.end();
+
+        auto rating_iter1 = ratings_.begin();
+        auto rating_iter2 = mv.rating_.begin();
+
+        float cond_a = 0;
+        float cond_b = 0;
+        float dot_product = 0;
+
+        while (first1 != last1 && first2 !=last2) {
+            if (*first1 < *first2) {
+                cond_a += *rating_iter1 * *rating_iter1;
+                ++first1;
+                ++rating_iter1;
+            } else if (*first2 < *first1) {
+                cond_b += *rating_iter2 * *rating_iter2;
+                ++first2;
+                ++rating_iter2;
+            } else {
+                dot_product += *rating_iter1 * rating_iter2;
+                cond_a += *rating_iter1 * *rating_iter1;
+                cond_b += *rating_iter2 * *rating_iter2;
+                ++first1; ++first2;
+                ++rating_iter1; ++rating_iter2;
+            }
+        }
+
+        return dot_product / (1 + sqrt(cond_a * cond_b));
+    }
+
 private:
+    static std::string to_hex_string(const size_t x) {
+        char buff[32] = { 0 };
+        sprintf(buff, "%zx", x);
+        
+        return buff;
+    }
+
     uint32_t movie_id_; 
     std::vector<uint32_t> user_ids_;
     std::vector<uint8_t> ratings_;
