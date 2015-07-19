7a6c33553a134710f5418d27f8a6448e0c09cd08
Jamis <hoojamis@gmail.com>
2015-07-19 22:55:26 +0800

    update k means iteration

--- src/k_means_iteration.cc
+++ src/k_means_iteration.cc
@@ -12,5 +12,157 @@
  *  Time: 20:55:38
  *  Description: 
  *****************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <cassert>
+
+#include "hadoop/Pipes.hh"
+#include "hadoop/TemplateFactory.hh"
+#include "hadoop/StringUtils.hh"
+
+#include "netflix_movie.h"
+
+constexpr size_t canopy_threshold = 2;
+const std::string canopy_centers_path = "canopy_output";
+const std::string k_means_center_path = "k_means_center";
+
+inline std::string to_hex_string(const size_t x) {
+    char buff[32] = { 0 };
+    sprintf(buff, "%zx", x);
+    
+    return buff;
+}
+
+inline std::vector<uint32_t> string_split(const std::string& str) {
+    std::vector<uint32_t> numbers;
+
+    char* offset;
+
+    const char* end = str.data() + str.length();
+    const char* tmp = str.data();
+    while (tmp < end) {
+        uint32_t movie_id = strtoul(tmp, &offset, 16);
+        numbers.emplace_back(movie_id);
+        tmp = offset + 1;
+    }
+
+    return numbers;
+}
+
+class kMeansMapper: public HadoopPipes::Mapper {
+public:
+    kMeansMapper(HadoopPipes::TaskContext& /* context */) {
+        load_canopy_centers();
+        load_kmeans_centers();
+
+        // no use anymore
+        canopy_centers.clear();
+    }
+
+    void map(HadoopPipes::MapContext& context) {
+        std::string input_value = context.getInputValue(); 
+
+        std::string movie_string = 
+            input_value.substr(0, input_value.find_first_of('\t')) + ':'
+            input_value.substr(input_value.find_first_of(';') + 1);
+
+        Movie movie = movie_string;
+
+        size_t start_pos = input_value.find_first_of('\t');
+        size_t end_pos = input_value.find_first_of(';', start_pos);
+        std::vector<uint32_t> canopy_ids = string_split(input_value.substr(start_pos + 1, end_pos - start_pos - 1));
+
+        float max_distance = -1;
+        Movie* max_distance_movie = nullptr;
+        for (const auto canopy_id: canopy_ids) {
+            if (canopy_id == movie.movie_id()) continue;
+            for (const auto& k_means_center: centers[canopy_id]) {
+                float distance = movie.cos_distance(k_means_center);
+                if (distance > max_distance) {
+                    max_distance = distance;
+                    max_distance_movie = &k_means_center;
+                }
+            }
+        }
+
+        std::string emit_key = max_distance_movie->to_string();
+        std::string emit_value = movie_string;
+
+        context.emit(emit_key, emit_value);
+    }
+
+private:   
+    void load_canopy_centers() {
+        std::ifstream fin(canopy_centers_path);
+
+        std::string line;
+        while (std::getline(fin, line)) 
+            canopy_centers.emplace_back(line);
+
+    }
+
+    void load_kmeans_centers() {
+        std::ifstream fin(k_means_center_path);
+
+        std::string line;
+        while (std::getline(fin, line)) {
+            Movie k_means_center(line);
+
+            for (const auto& canopy_center: canopy_centers) 
+                if (k_means_center.user_match_count(canopy_center) > canopy_threshold) {
+                    auto ite = centers.emplace(canopy_center.movie_id(), { }).first;
+                    ite->second.push_back(k_means_center);
+                }
+        }
+    }
+    
+    std::vector<Movie> canopy_centers;
+    // canopy movie_id, vector of k-means centers
+    std::unordered_map< uint32_t, std::vector<Movie> > centers;
+};
+
+class kMeansReducer: public HadoopPipes::Reducer {
+public:
+    kMeansReducer(const HadoopPipes::TaskContext& /* context */) { }
+
+    void reduce(HadoopPipes::ReduceContext& context) {
+        Movie k_means_center(context.getInputKey());
+
+        // <user id, < number of users, total user ratings > >
+        std::unordered_map<uint32_t, std::pair<uint32_t, uint32_t> > new_features:;
+
+        while (context.nextValue()) {
+            Movie movie(contet.getInputValue());
+
+            for (size_t i = 0; i < movie.num_users(); ++i) {
+                auto ite = new_features.emplace(movie.user_id(i), { 0, 0 }).first;
+
+                ite->second.first += 1;
+                ite->second.second += movie.rating(i);
+            }
+        }
+
+        std::vector< std::pair<uint32_t, uint32_t> > new_features_vec;
+        for (const auto i: new_features)
+            new_features_vec.push_back({ i.first, i.second.second / i.second.first });
+
+        sort(new_features_vec.begin(), new_features_vec.end());
+
+        std::string emit_key = to_hex_string(k_means_center.movie_id());
+
+        std::string emit_value;
+        for (const auto i: new_features_vec)
+            emit_value += to_hex_string(i.first) + ',' + to_hex_string(i.second) + ',';
+
+        if (emit_value.size()) {
+            emit_value.pop_back();
+            context.emit(emit_key, emit_value);
+        }
+    }
+};
+
+int main(int, char**) {
+    return HadoopPipes::runTask(HadoopPipes::TemplateFactory<kMeansMapper, kMeansReducer>());
+}
 
 
