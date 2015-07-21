1c06a62dded8714d8bc377b7b6fc2239963c24b8
Jamis <hoojamis@gmail.com>
2015-07-21 21:03:07 +0800

    update submit script

--- submit.sh
+++ submit.sh
@@ -23,63 +23,63 @@ K_MEANS_CENTERS=k_means_centers
 K_MEANS_RESULT=k_means_result
 k_ITERATIONS=4
 
-## Step 0: update exec and data
-#${HADOOP_HOME}/bin/hdfs dfs -mkdir bin/
-#${HADOOP_HOME}/bin/hdfs dfs -put canopy_clustering bin/
-#${HADOOP_HOME}/bin/hdfs dfs -put label_data bin/
-#${HADOOP_HOME}/bin/hdfs dfs -put k_means_iteration bin/
-#${HADOOP_HOME}/bin/hdfs dfs -put k_means_last_iteration bin/
-#${HADOOP_HOME}/bin/hdfs dfs -mkdir $ORIGINAL_INPUT_PATH
-#for i in data/${ORIGINAL_INPUT_PATH}_blocks/*; do
-#    ${HADOOP_HOME}/bin/hdfs dfs -put $i $ORIGINAL_INPUT_PATH/
-#done
-#
-#echo "Step 0 success. "
-#
-## Step 1: get canopy centers
-#${HADOOP_HOME}/bin/mapred pipes \
-#-conf src/conf.xml \
-#-D mapred.reduce.tasks=1 \
-#-input $ORIGINAL_INPUT_PATH \
-#-output $CANOPY_CENTERS \
-#-program bin/canopy_clustering
-#
-#if (($?)); then
-#    echo "Step 1 failed. "
-#    exit 1
-#fi
-#
-#echo "Step 1 success. "
+# Step 0: update exec and data
+${HADOOP_HOME}/bin/hdfs dfs -mkdir bin/
+${HADOOP_HOME}/bin/hdfs dfs -put canopy_clustering bin/
+${HADOOP_HOME}/bin/hdfs dfs -put label_data bin/
+${HADOOP_HOME}/bin/hdfs dfs -put k_means_iteration bin/
+${HADOOP_HOME}/bin/hdfs dfs -put k_means_last_iteration bin/
+${HADOOP_HOME}/bin/hdfs dfs -mkdir $ORIGINAL_INPUT_PATH
+for i in data/${ORIGINAL_INPUT_PATH}_blocks/*; do
+    ${HADOOP_HOME}/bin/hdfs dfs -put $i $ORIGINAL_INPUT_PATH/
+done
 
-#rm -rf data/$CANOPY_CENTERS
-#${HADOOP_HOME}/bin/hdfs dfs -get $CANOPY_CENTERS/part-00000 data/$CANOPY_CENTERS
-#
-## Step 2: label all data with canopy centers
-#${HADOOP_HOME}/bin/mapred pipes \
-#-conf src/conf.xml \
-#-files data/$CANOPY_CENTERS \
-#-input $ORIGINAL_INPUT_PATH \
-#-output $CANOPY_LABELED \
-#-program bin/label_data
-#
-#if (($?)); then
-#    echo "Step 2 failed. "
-#    exit 1
-#fi
-#
-#echo "Step 2 success. "
+echo "Step 0 success. "
 
-# Step 3: k means iteration
-#rm -rf data/$CANOPY_LABELED
-#${HADOOP_HOME}/bin/hdfs dfs -get $CANOPY_LABELED/part-00000 data/$CANOPY_LABELED
-#if [ -e data/$CANOPY_LABELED ]; then
-#    ${HADOOP_HOME}/bin/hdfs dfs -rm $CANOPY_LABELED/*
-#fi
-#rm -rf data/${CANOPY_LABELED}_blocks
-#python3 data/split_blocks.py data/$CANOPY_LABELED data/${CANOPY_LABELED}_blocks/
-#for i in data/${CANOPY_LABELED}_blocks/*; do
-#    ${HADOOP_HOME}/bin/hdfs dfs -put $i $CANOPY_LABELED/
-#done
+# Step 1: get canopy centers
+${HADOOP_HOME}/bin/mapred pipes \
+-conf src/conf.xml \
+-D mapred.reduce.tasks=1 \
+-input $ORIGINAL_INPUT_PATH \
+-output $CANOPY_CENTERS \
+-program bin/canopy_clustering
+
+if (($?)); then
+    echo "Step 1 failed. "
+    exit 1
+fi
+
+echo "Step 1 success. "
+
+rm -rf data/$CANOPY_CENTERS
+${HADOOP_HOME}/bin/hdfs dfs -get $CANOPY_CENTERS/part-00000 data/$CANOPY_CENTERS
+
+# Step 2: label all data with canopy centers
+${HADOOP_HOME}/bin/mapred pipes \
+-conf src/conf.xml \
+-files data/$CANOPY_CENTERS \
+-input $ORIGINAL_INPUT_PATH \
+-output $CANOPY_LABELED \
+-program bin/label_data
+
+if (($?)); then
+    echo "Step 2 failed. "
+    exit 1
+fi
+
+echo "Step 2 success. "
+
+ Step 3: k means iteration
+rm -rf data/$CANOPY_LABELED
+${HADOOP_HOME}/bin/hdfs dfs -get $CANOPY_LABELED/part-00000 data/$CANOPY_LABELED
+if [ -e data/$CANOPY_LABELED ]; then
+    ${HADOOP_HOME}/bin/hdfs dfs -rm $CANOPY_LABELED/*
+fi
+rm -rf data/${CANOPY_LABELED}_blocks
+python3 data/split_blocks.py data/$CANOPY_LABELED data/${CANOPY_LABELED}_blocks/
+for i in data/${CANOPY_LABELED}_blocks/*; do
+    ${HADOOP_HOME}/bin/hdfs dfs -put $i $CANOPY_LABELED/
+done
 
 
 cp data/$K_MEANS_INITIAL_CENTERS data/$K_MEANS_CENTERS
@@ -114,7 +114,7 @@ done
 
 # Step 4: last k means iteration, output clustering result rather than k-means centers
 
-${HADOOP_HOEM}/bin/mapred pipes \
+${HADOOP_HOME}/bin/mapred pipes \
 -conf src/conf.xml \
 -files data/$CANOPY_CENTERS,data/$K_MEANS_CENTERS \
 -input $CANOPY_LABELED \
